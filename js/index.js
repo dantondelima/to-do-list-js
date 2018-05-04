1ae31398c84972e19853f5787f7e014d79d70fe8
dantondelima <dantondelima.contato@gmail.com>
2018-05-04 10:00:01 -0300

    carousel modificações pagina inicial

--- js/index.js
+++ js/index.js
@@ -1,19 +1,61 @@
-var slideIndex = 0;
-showSlides();
+var slideIndex = 2;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    slideIndex++;
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    
+    if (slideIndex > x.length) { 
+        slideIndex = 1 
+    }
+
+    if (slideIndex < 1) { 
+        slideIndex = x.length 
+    }
+    
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showDivs, 4000) // Change image every 4 seconds
+}
+
+
+/*
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
+    if (slideIndex > slides.length) { 
+        slideIndex = 1; 
+    }
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
+
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+*/
